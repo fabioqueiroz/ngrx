b1bd26dc0d9422a9ece3bb2f398e2850656d94a7
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2020-01-06 23:02:02 +0000

    update operations completed

--- APM-Demo0/src/app/products/state/product.effects.ts
+++ APM-Demo0/src/app/products/state/product.effects.ts
@@ -4,7 +4,8 @@ import { ProductService } from "../product.service";
 import * as productActions from "./product.actions";
 import { mergeMap, map, catchError } from "rxjs/operators";
 import { Product } from "../product";
-import { of } from "rxjs";
+import { of, Observable } from "rxjs";
+import { Action } from '@ngrx/store'
 
 
 @Injectable() 
@@ -14,11 +15,23 @@ export class ProductEffects {
                 private _productService: ProductService){}
     
     @Effect()
-    loadProducts$ = this._actions$.pipe(
+    loadProducts$: Observable<Action> = this._actions$.pipe(
         ofType(productActions.ProductActionTypes.Load),
         mergeMap((action: productActions.Load) => this._productService.getProducts().pipe(
             map((products: Product[]) => (new productActions.LoadSuccess(products))),
             catchError(err => of(new productActions.LoadFail(err)))
         ))
+    );
+
+    @Effect()
+    updateProduct$: Observable<Action> = this._actions$.pipe(
+        ofType(productActions.ProductActionTypes.UpdateProduct),
+        map((action: productActions.UpdateProduct) => action.payload),
+        mergeMap((product: Product) => 
+            this._productService.updateProduct(product).pipe(
+                map(updatedProduct => (new productActions.UpdateProductSuccess(updatedProduct))),
+                catchError(err => of(new productActions.UpdateProductFail(err)))
+            )
+        )
     );
 }
